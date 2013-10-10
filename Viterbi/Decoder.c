b0a9c6bb1ea56f9e588c387d839bbf84fa584305
Kristofer Leifland <kristofer.leifland@gmail.com>
2013-10-10 10:50:25 +0200

    Implemented decoder initialization.

--- Viterbi/Decoder.c
+++ Viterbi/Decoder.c
@@ -1,8 +1,34 @@
 #include "Decoder.h"
-
+#include "Encoder.h"
 
 
 Data viterbi_decoder(Message data) {
-  int accum_err_metric[4][2];
+  int input[NUMBER_OF_STATES][NUMBER_OF_STATES]; /* maps current/nxt sts to input */
+  int accum_err_metric[NUMBER_OF_STATES][2];
+  int depth_of_trellis;
+  int nextstate[NUMBER_OF_STATES][2]; /* for current st, gives nxt given input */
+  int output[NUMBER_OF_STATES][2]; /* gives conv. encoder output */
+  int next_state;
+  int n, i, j;
+
+  /* n is 2^1 = 2 for rate 1/2 */
+  n = 2;
+  
+  depth_of_trellis = K * 5;
+
+  /* initialize */
+  for (i = 0; i < NUMBER_OF_STATES; ++i) {
+    for (j = 0; j < NUMBER_OF_STATES; ++j)
+      input[i][j] = 0;
+    
+    for (j = 0; j < n; j++) {
+      nextstate[i][j] = 0;
+      output[i][j] = 0;
+    }
+
+    accum_err_metric[i][0] = 0;
+    accum_err_metric[i][1] = INT_MAX;
+  }
+
 
 }
--- Viterbi/Decoder.h
+++ Viterbi/Decoder.h
@@ -1,6 +1,7 @@
 #ifndef VITERBI_DECODER_H
 #define VITERBI_DECODER_H
 
+#include <limits.h>
 #include "Data.h"
 
 Data viterbi_decoder(Message data);
