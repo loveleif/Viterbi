1c897186f8faf9b3abee10d029621ead4cd7d52b
Kristofer Leifland <kristofer.leifland@gmail.com>
2013-10-10 10:48:14 +0200

    Refactoring.

--- Viterbi/Encoder.c
+++ Viterbi/Encoder.c
@@ -1,14 +1,14 @@
 #include "Encoder.h"
 #include<stdio.h>
 
-int falt_output_table[4][2] = { 
+int output_table[4][2] = { 
 // Input: 0  1
 		 {0, 3}, // Current state = 00
 		 {3, 0}, // Current state = 01
 		 {2, 1}, // Current state = 10
 		 {1, 2}  // Current state = 11 
 };
-int falt_state_table[4][2] = { 
+int next_state_table[4][2] = { 
 // Input: 0  1
 		   {0, 2}, // Current state = 00
 	     {0, 2}, // Current state = 01
@@ -22,8 +22,8 @@ Message falt_encoder(Data data) {
 
 	for (i = 7; i != -1; --i) {
 		input = (data >> i) & 1;
-		state = falt_state_table[state][input];
-		output = falt_output_table[state][input];
+		state = next_state_table[state][input];
+		output = next_state_table[state][input];
 		message |= output << 2*i;
 	}
 	return message;
