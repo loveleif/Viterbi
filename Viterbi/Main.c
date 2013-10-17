ff0ca1f1e672b072b48cc7001488b08ac09140ce
Kristofer Leifland <kristofer.leifland@gmail.com>
2013-10-17 15:21:34 +0200

    Added test in main.

--- Viterbi/Main.c
+++ Viterbi/Main.c
@@ -4,8 +4,28 @@
 #include "Util.h"
 #include "Decoder.h"
 #include "DecoderBrute.h"
+
+/* Would like a semi-open interval [min, max) */
+int random_in_range(unsigned int min, unsigned int max)
+{
+	  int range       = max - min,
+      remainder   = RAND_MAX % range,
+      bucket      = RAND_MAX / range;
+	int base_random = rand(); /* in [0, RAND_MAX] */
+  if (RAND_MAX == base_random) return random_in_range(min, max);
+  /* now guaranteed to be in [0, RAND_MAX) */
+
+  /* There are range buckets, plus one smaller interval
+     within remainder of RAND_MAX */
+  if (base_random < RAND_MAX - remainder) {
+    return min + base_random/bucket;
+  } else {
+    return random_in_range (min, max);
+  }
+}
+
 int main() {
-  int in, out;
+  int i, in, out, outerr, decoded, errors = 0;
   /*
   in = 0; out = falt_encoder(in);
   printf("%d -> %d\n", in, out);
@@ -28,12 +48,25 @@ int main() {
   printf("%s\n", byte_to_binary(out));
   */
 
+  for (i = 0; i < 10000; ++i) {
+	  in = random_in_range(0, 0xFF);
+	  out = memory_falt_encoder(in);
+	  outerr = out ^ (1 << random_in_range(0,8));
+	  outerr = outerr ^ (1 << random_in_range(0,8));
+	  decoded = brute_force_viterbi_decoder(outerr);
+	  if (decoded != in) ++errors;
+	  //printf("decoded: %d   in: %d   outerr: %d\n", decoded, in, inerr);
+  }
+  printf("Errors: %d", errors);
+  /*
   in = 0x5C; out = falt_encoder(in);
   printf("%s -> ", byte_to_binary(in, 8));
   printf("%s\n", byte_to_binary(out, 20));
 
   printf("  Kod: %s\n", byte_to_binary(in, 8));
   printf("Avkod: %s\n", byte_to_binary(brute_force_viterbi_decoder(out), 8));
+  */
+
 
   //viterbi_decoder(0x1);
 
